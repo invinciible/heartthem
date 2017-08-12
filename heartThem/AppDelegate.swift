2be6cad621eb194b500fc68f155f24b90fee292d
invinciible <tktusharkatyal11@gmail.com>
2017-08-12 13:32:06 +0530

    Facebook auth completed with firebase

--- heartThem/AppDelegate.swift
+++ heartThem/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import  Firebase
+import FBSDKLoginKit
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         return true
     }
 
@@ -41,6 +47,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+   
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application,open: url ,sourceApplication: sourceApplication,annotation: annotation)
+        
+    }
 
 }
 
--- heartThem/SignInVC.swift
+++ heartThem/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,37 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func fbLoginBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            
+            
+            if error != nil {
+                
+                print("Tush : unable to authentical with facebook - \(error ?? "Error authenticating" as! Error)")
+            } else if result?.isCancelled == true {
+                
+                print("Tush : user cancelled fb suthentication")
+            } else {
+                print("Tush :successfully authenticate with facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+                }
+            }
+        }
 
+    func firebaseAuth(_ credential : AuthCredential) {
+        
+        Auth.auth().signIn(with: credential) { (user, error) in
+            
+            if error != nil {
+                print("Tush : unable to authenticate with firebase")
+            } else {
+                print("tush : successfully authenticate with firebase")
+            }
+        }
+    }
 }
 
